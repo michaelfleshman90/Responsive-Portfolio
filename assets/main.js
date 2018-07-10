45d8d0349053cbe750f5dc63c752dcbc61b531a7
Michael Fleshman <michaelfleshman90@gmail..com>
2018-07-09 21:39:10 -0400

    push

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+function aboutbutton() {
+    window.location.href="index.html";
+}
+function contactbutton() {
+    window.location.href="contact.html";
+}
+function portfoliobutton() {
+    window.location.href="portfolio.html";
+}
+function portbutton1() {
+    window.location.href="https://www.google.com/";
+}
+function portbutton2() {
+    window.location.href="https://google.com/";
+}
+function portbutton3() {
+    window.location.href="https://google.com/";
+}
+function portbutton4() {
+    window.location.href="https://google.com/";
+}
+function portbutton5() {
+    window.location.href="https://google.com/";
+}
